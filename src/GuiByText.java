4f24997a094e5ca33715d47382692298648f5fba
anastasyaokta <andreiya6@gmail.com>
2021-06-12 12:31:31 +0800

    GuiByText

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -3,7 +3,11 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     GuiByText(){
        setSize(640,480);
+       setLocationRelativeTo(null);
+       setTitle("GUI By Text");
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
 
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
